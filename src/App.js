a6589bd13aa68373447864a3b200d38c041385da
Charles Floyd <charleswfloyd3@gmail.com>
2021-02-28 15:19:35 -0800

    testing the build version...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link}  from 'react-router-dom';
 
 import Axios from 'axios'
 import './App.css';
