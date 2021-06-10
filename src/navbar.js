a21dedfb59e790ef376ae77afb404e7c41f438c6
Charles Floyd <charleswfloyd3@gmail.com>
2021-06-10 00:56:30 -0600

    changed navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import './navbar.css'
 function Navbar(props){
+    const [dropdown, setdropdown] = useState(true)
 
     return(
         <nav className="navbar">
-            
-            <p className="companyName"><img className="companySymbol" src={props.coinImage} ></img></p>
-            <button className="signin_btn" onClick={props.searchInfo}><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/">Home</Link></button>
-                        <button className="signup_btn" onClick={props.searchInfo}><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/">Track</Link></button>
-            <button className="signup_btn" onClick={props.searchInfo}><Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/">Search</Link></button>
-
+            <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/"><div className="nav-left">
+                <p className="cryptobase">Cryptobase</p>
+                <img src="images/btclogo.png" className="companyLogo"></img>
+            </div></Link>
+            <div className="nav-right">
+                <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/"><p className="search">Search</p></Link>
+                <p className='getStartedBtn'>Get Started</p>
+            </div>
+            {dropdown ? <ul className="hamburgerIcon" onClick={()=>{setdropdown(false)}}>&#9776;</ul>: <div><ul className="hamburgerIcon" onClick={()=>{setdropdown(true)}}>&#10006;</ul> <div className="dropdown">
+                 <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/"><p className="searchDrop">Search</p></Link>
+                <p className='getStartedBtnDrop'>Get Started</p>
+            </div></div>}
 
         </nav>
     )
